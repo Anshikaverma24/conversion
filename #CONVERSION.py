67fae2bdb662bc7ccd904e8cac56bd95c8f4f798
Anshikaverma24 <90278284+Anshikaverma24@users.noreply.github.com>
2021-09-07 23:31:09 +0530

    Add files via upload

---
+++ #CONVERSION.py
@@ -0,0 +1,21 @@
+# CONVERSION
+a=4
+b="16"
+c=int(b)
+print(a+c)
+
+var12="5"
+var13=float(var12)
+print(var13)
+var23=2.8
+var24=int(var23)
+print(var24)
+var34=24
+var35=str(var34)
+print(var35)
+var45=2.9
+var46=str(var45)
+print(var46)
+var56="hello"
+var57=int(var56)
+print(var57)
